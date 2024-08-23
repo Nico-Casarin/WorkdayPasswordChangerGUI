27e984f7ba8609c88229f47f6bd8df2591dda887
Nico-Casarin <nico.casarin@gmail.com>
2024-08-23 18:29:14 +0200

    Configuration form fix placement and uniformed design

--- PasswordChanger/Configuration.Designer.cs
+++ PasswordChanger/Configuration.Designer.cs
@@ -33,26 +33,25 @@
             flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             btn_Discard = new System.Windows.Forms.Button();
             btn_Save = new System.Windows.Forms.Button();
-            flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel_CSVPath = new System.Windows.Forms.FlowLayoutPanel();
             lbl_Password = new System.Windows.Forms.Label();
             text_Passwd = new System.Windows.Forms.TextBox();
-            flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel_Psw = new System.Windows.Forms.FlowLayoutPanel();
             lbl_Path = new System.Windows.Forms.Label();
             text_Path = new System.Windows.Forms.TextBox();
-            flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel_Username = new System.Windows.Forms.FlowLayoutPanel();
             lbl_Username = new System.Windows.Forms.Label();
             text_Username = new System.Windows.Forms.TextBox();
-            checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             label1 = new System.Windows.Forms.Label();
-            flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            flowLayoutPanel_APIURL = new System.Windows.Forms.FlowLayoutPanel();
             label2 = new System.Windows.Forms.Label();
             text_API = new System.Windows.Forms.TextBox();
             tableLayoutPanel1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
-            flowLayoutPanel4.SuspendLayout();
-            flowLayoutPanel2.SuspendLayout();
-            flowLayoutPanel3.SuspendLayout();
-            flowLayoutPanel5.SuspendLayout();
+            flowLayoutPanel_CSVPath.SuspendLayout();
+            flowLayoutPanel_Psw.SuspendLayout();
+            flowLayoutPanel_Username.SuspendLayout();
+            flowLayoutPanel_APIURL.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -60,25 +59,24 @@
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 7);
-            tableLayoutPanel1.Controls.Add(flowLayoutPanel4, 0, 2);
-            tableLayoutPanel1.Controls.Add(flowLayoutPanel2, 0, 3);
-            tableLayoutPanel1.Controls.Add(flowLayoutPanel3, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel_CSVPath, 0, 2);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel_Psw, 0, 2);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel_Username, 0, 1);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
-            tableLayoutPanel1.Controls.Add(flowLayoutPanel5, 0, 4);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel_APIURL, 0, 4);
             tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 8;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.45455F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36364F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.4964962F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5005007F));
             tableLayoutPanel1.Size = new System.Drawing.Size(476, 359);
             tableLayoutPanel1.TabIndex = 0;
             tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
@@ -89,10 +87,10 @@
             flowLayoutPanel1.Controls.Add(btn_Save);
             flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            flowLayoutPanel1.Location = new System.Drawing.Point(4, 316);
+            flowLayoutPanel1.Location = new System.Drawing.Point(4, 311);
             flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new System.Drawing.Size(468, 40);
+            flowLayoutPanel1.Size = new System.Drawing.Size(468, 45);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // btn_Discard
@@ -117,78 +115,78 @@
             btn_Save.UseVisualStyleBackColor = true;
             btn_Save.Click += btn_Save_Click;
             // 
-            // flowLayoutPanel4
+            // flowLayoutPanel_CSVPath
             // 
-            flowLayoutPanel4.Controls.Add(lbl_Password);
-            flowLayoutPanel4.Controls.Add(text_Passwd);
-            flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            flowLayoutPanel4.Location = new System.Drawing.Point(4, 83);
-            flowLayoutPanel4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            flowLayoutPanel4.Name = "flowLayoutPanel4";
-            flowLayoutPanel4.Size = new System.Drawing.Size(468, 34);
-            flowLayoutPanel4.TabIndex = 3;
+            flowLayoutPanel_CSVPath.Controls.Add(lbl_Path);
+            flowLayoutPanel_CSVPath.Controls.Add(text_Path);
+            flowLayoutPanel_CSVPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            flowLayoutPanel_CSVPath.Location = new System.Drawing.Point(4, 135);
+            flowLayoutPanel_CSVPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            flowLayoutPanel_CSVPath.Name = "flowLayoutPanel_CSVPath";
+            flowLayoutPanel_CSVPath.Size = new System.Drawing.Size(468, 38);
+            flowLayoutPanel_CSVPath.TabIndex = 3;
             // 
             // lbl_Password
             // 
             lbl_Password.Anchor = System.Windows.Forms.AnchorStyles.None;
-            lbl_Password.AutoSize = true;
             lbl_Password.Location = new System.Drawing.Point(4, 7);
             lbl_Password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             lbl_Password.Name = "lbl_Password";
-            lbl_Password.Size = new System.Drawing.Size(57, 15);
+            lbl_Password.Size = new System.Drawing.Size(60, 15);
             lbl_Password.TabIndex = 1;
             lbl_Password.Text = "Password";
             // 
             // text_Passwd
             // 
-            text_Passwd.Location = new System.Drawing.Point(69, 3);
+            text_Passwd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            text_Passwd.Location = new System.Drawing.Point(72, 3);
             text_Passwd.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             text_Passwd.Name = "text_Passwd";
             text_Passwd.PasswordChar = '*';
-            text_Passwd.Size = new System.Drawing.Size(386, 23);
+            text_Passwd.Size = new System.Drawing.Size(383, 23);
             text_Passwd.TabIndex = 2;
             text_Passwd.TextChanged += text_Passwd_TextChanged;
             // 
-            // flowLayoutPanel2
+            // flowLayoutPanel_Psw
             // 
-            flowLayoutPanel2.Controls.Add(lbl_Path);
-            flowLayoutPanel2.Controls.Add(text_Path);
-            flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            flowLayoutPanel2.Location = new System.Drawing.Point(4, 123);
-            flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            flowLayoutPanel2.Name = "flowLayoutPanel2";
-            flowLayoutPanel2.Size = new System.Drawing.Size(468, 67);
-            flowLayoutPanel2.TabIndex = 4;
+            flowLayoutPanel_Psw.Controls.Add(lbl_Password);
+            flowLayoutPanel_Psw.Controls.Add(text_Passwd);
+            flowLayoutPanel_Psw.Dock = System.Windows.Forms.DockStyle.Fill;
+            flowLayoutPanel_Psw.Location = new System.Drawing.Point(4, 91);
+            flowLayoutPanel_Psw.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            flowLayoutPanel_Psw.Name = "flowLayoutPanel_Psw";
+            flowLayoutPanel_Psw.Size = new System.Drawing.Size(468, 38);
+            flowLayoutPanel_Psw.TabIndex = 4;
             // 
             // lbl_Path
             // 
             lbl_Path.Anchor = System.Windows.Forms.AnchorStyles.None;
-            lbl_Path.AutoSize = true;
             lbl_Path.Location = new System.Drawing.Point(4, 7);
             lbl_Path.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             lbl_Path.Name = "lbl_Path";
-            lbl_Path.Size = new System.Drawing.Size(55, 15);
+            lbl_Path.Size = new System.Drawing.Size(60, 15);
             lbl_Path.TabIndex = 1;
             lbl_Path.Text = "CSV Path";
             // 
             // text_Path
             // 
-            text_Path.Location = new System.Drawing.Point(67, 3);
+            text_Path.Anchor = System.Windows.Forms.AnchorStyles.None;
+            text_Path.Location = new System.Drawing.Point(72, 3);
             text_Path.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             text_Path.Name = "text_Path";
-            text_Path.Size = new System.Drawing.Size(386, 23);
+            text_Path.Size = new System.Drawing.Size(383, 23);
             text_Path.TabIndex = 2;
             // 
-            // flowLayoutPanel3
+            // flowLayoutPanel_Username
             // 
-            flowLayoutPanel3.Controls.Add(lbl_Username);
-            flowLayoutPanel3.Controls.Add(text_Username);
-            flowLayoutPanel3.Controls.Add(checkedListBox1);
-            flowLayoutPanel3.Location = new System.Drawing.Point(4, 43);
-            flowLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            flowLayoutPanel3.Name = "flowLayoutPanel3";
-            flowLayoutPanel3.Size = new System.Drawing.Size(468, 32);
-            flowLayoutPanel3.TabIndex = 2;
+            flowLayoutPanel_Username.Anchor = System.Windows.Forms.AnchorStyles.None;
+            flowLayoutPanel_Username.Controls.Add(lbl_Username);
+            flowLayoutPanel_Username.Controls.Add(text_Username);
+            flowLayoutPanel_Username.Location = new System.Drawing.Point(4, 50);
+            flowLayoutPanel_Username.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            flowLayoutPanel_Username.Name = "flowLayoutPanel_Username";
+            flowLayoutPanel_Username.Size = new System.Drawing.Size(468, 32);
+            flowLayoutPanel_Username.TabIndex = 2;
             // 
             // lbl_Username
             // 
@@ -204,27 +202,19 @@
             // 
             // text_Username
             // 
+            text_Username.Anchor = System.Windows.Forms.AnchorStyles.None;
             text_Username.Location = new System.Drawing.Point(72, 3);
             text_Username.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             text_Username.Name = "text_Username";
             text_Username.Size = new System.Drawing.Size(383, 23);
             text_Username.TabIndex = 1;
             // 
-            // checkedListBox1
-            // 
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new System.Drawing.Point(4, 32);
-            checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new System.Drawing.Size(139, 94);
-            checkedListBox1.TabIndex = 2;
-            // 
             // label1
             // 
             label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            label1.Location = new System.Drawing.Point(150, 4);
+            label1.Location = new System.Drawing.Point(150, 6);
             label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(176, 31);
@@ -232,34 +222,34 @@
             label1.Text = "Configuration";
             label1.Click += label1_Click_1;
             // 
-            // flowLayoutPanel5
+            // flowLayoutPanel_APIURL
             // 
-            flowLayoutPanel5.Controls.Add(label2);
-            flowLayoutPanel5.Controls.Add(text_API);
-            flowLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            flowLayoutPanel5.Location = new System.Drawing.Point(3, 196);
-            flowLayoutPanel5.Name = "flowLayoutPanel5";
-            flowLayoutPanel5.Size = new System.Drawing.Size(470, 34);
-            flowLayoutPanel5.TabIndex = 6;
+            flowLayoutPanel_APIURL.Controls.Add(label2);
+            flowLayoutPanel_APIURL.Controls.Add(text_API);
+            flowLayoutPanel_APIURL.Dock = System.Windows.Forms.DockStyle.Fill;
+            flowLayoutPanel_APIURL.Location = new System.Drawing.Point(3, 179);
+            flowLayoutPanel_APIURL.Name = "flowLayoutPanel_APIURL";
+            flowLayoutPanel_APIURL.Size = new System.Drawing.Size(470, 38);
+            flowLayoutPanel_APIURL.TabIndex = 6;
             // 
             // label2
             // 
             label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            label2.AutoSize = true;
             label2.Location = new System.Drawing.Point(4, 7);
             label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(49, 15);
+            label2.Size = new System.Drawing.Size(60, 15);
             label2.TabIndex = 2;
             label2.Text = "API URL";
             label2.Click += label2_Click;
             // 
             // text_API
             // 
-            text_API.Location = new System.Drawing.Point(61, 3);
+            text_API.Anchor = System.Windows.Forms.AnchorStyles.None;
+            text_API.Location = new System.Drawing.Point(72, 3);
             text_API.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             text_API.Name = "text_API";
-            text_API.Size = new System.Drawing.Size(386, 23);
+            text_API.Size = new System.Drawing.Size(384, 23);
             text_API.TabIndex = 3;
             text_API.TextChanged += textBox1_TextChanged;
             // 
@@ -277,36 +267,35 @@
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             flowLayoutPanel1.ResumeLayout(false);
-            flowLayoutPanel4.ResumeLayout(false);
-            flowLayoutPanel4.PerformLayout();
-            flowLayoutPanel2.ResumeLayout(false);
-            flowLayoutPanel2.PerformLayout();
-            flowLayoutPanel3.ResumeLayout(false);
-            flowLayoutPanel3.PerformLayout();
-            flowLayoutPanel5.ResumeLayout(false);
-            flowLayoutPanel5.PerformLayout();
+            flowLayoutPanel_CSVPath.ResumeLayout(false);
+            flowLayoutPanel_CSVPath.PerformLayout();
+            flowLayoutPanel_Psw.ResumeLayout(false);
+            flowLayoutPanel_Psw.PerformLayout();
+            flowLayoutPanel_Username.ResumeLayout(false);
+            flowLayoutPanel_Username.PerformLayout();
+            flowLayoutPanel_APIURL.ResumeLayout(false);
+            flowLayoutPanel_APIURL.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button btn_Discard;
         private System.Windows.Forms.Button btn_Save;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
-        private System.Windows.Forms.Label lbl_Password;
-        private System.Windows.Forms.Label lbl_Path;
-        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_Username;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_CSVPath;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_Psw;
         private System.Windows.Forms.TextBox text_Passwd;
         private System.Windows.Forms.TextBox text_Path;
         private System.Windows.Forms.TextBox text_Username;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_APIURL;
         private System.Windows.Forms.TextBox text_API;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lbl_Path;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Password;
     }
 }
